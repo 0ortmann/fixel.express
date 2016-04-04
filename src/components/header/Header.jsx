8319e7e3b84ff76d0bf9f51bf6f4878640440085
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2016-04-05 00:19:04 +0200

    play with css animations, header.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,26 @@ export default class Header extends Component {
     render() {
         return (
             <header>
-                <div className='header__train'>
+                <div className='header__fixel'>
+                    <div className='fixel__smoke'> 
+                        <span className='fixel__smoke__0'></span>
+                        <span className='fixel__smoke__1'></span>
+                        <span className='fixel__smoke__2'></span>
+                        <span className='fixel__smoke__3'></span>
+                    </div>
+                    <div className='fixel__jumping'>F</div>
+                    <div className='fixel__jumping'>I</div>
+                    <div className='fixel__jumping'>X</div>
+                    <div className='fixel__jumping'>E</div>
+                    <div className='fixel__jumping'>L</div>
+
+                    <div className='express__e1 fixel__jumping'>E</div>
+                    <div className='fixel__jumping'>X</div>
+                    <div className='fixel__jumping'>P</div>
+                    <div className='fixel__jumping'>R</div>
+                    <div className='fixel__jumping'>E</div>
+                    <div className='fixel__jumping'>S</div>
+                    <div className='fixel__jumping'>S</div>
                 </div>
             </header>
         );

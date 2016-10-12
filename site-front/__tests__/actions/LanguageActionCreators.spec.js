d2ae5c1b9390277501289e61981e53d1c234e21d
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2016-10-12 12:36:25 +0200

    actioncreators tests

---
+++ site-front/__tests__/actions/LanguageActionCreators.spec.js
@@ -0,0 +1,23 @@
+import TestUtils from 'react-addons-test-utils';
+import ACTIONS from '../../src/constants/Constants';
+
+import { getLanguage } from '../../src/actions/LanguageActionCreators.js';
+
+describe('LanguageActionCreators', () => {
+
+	it('should construct a fetch-action for the requested language', () => {
+
+		const action = getLanguage('LANGUAGE');
+
+		const expectedUrl = '/lang/LANGUAGE.json'; 
+
+		const expectedAction = {
+			[ACTIONS.CALL_API]: {
+				types: [ACTIONS.GET_LANGUAGE, ACTIONS.GET_LANGUAGE_SUCCESS, ACTIONS.GET_LANGUAGE_ERROR],
+				endpoint: expectedUrl,
+				host: 'local'
+			}
+		}
+		expect(action).toEqual(expectedAction);
+	});
+});

7be766c2f9f677ec8e46f1202105b263b0a8eb4e
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2017-07-20 21:47:45 +0200

    meta description

--- site-front/server/server.js
+++ site-front/server/server.js
@@ -77,6 +77,7 @@ function renderFullPage(html, preloadedState) {
 		<head>
 			<meta charset='utf-8'>
 			<meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable=yes" />
+			<meta name="description" content="Homepage of Felix Ortmann. I love code." />
 			<title>Fixel Express</title>
 			<link rel='stylesheet' type='text/css' href='/assets/bundle.css'>
 			<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
